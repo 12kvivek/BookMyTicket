e51dd75ac9124ddcbb268735d19356f3706d5271
12kvivek <vivekkakoo66@gmail.com>
2023-09-05 16:21:01 +0530

    commit

--- src/login.js
+++ src/login.js
@@ -6,9 +6,18 @@ const login = () => {
     <div>
     <Navbar />
     <div class="lg">
-    <div class="login"> 
-        Click Here to Login or Signup
-    </div>
+    <div class="login-container">
+        <h1>Login/Signup</h1>
+        <form id="login-form">
+            <input type="text" id="username" placeholder="Username" required />
+            <input type="password" id="password" placeholder="Password" required />
+            <button type="submit">Login</button>
+        </form>
+        <div class="social-login">
+            <button id="facebook-login">Login with Facebook</button>
+            <button id="google-login">Login with Google</button>
+        </div>
+        </div>
     </div>
     </div>
   )
